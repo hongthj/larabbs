8155868f61818c65a9341edaf0d7a1419ce98cc6
terryhong <hongtianjin@chinamcloud.com>
2018-05-17 13:13:33 +0800

    change jwt to passport

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -7,6 +7,11 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\Api\SocialAuthorizationRequest;
 
+use Zend\Diactoros\Response as Psr7Response;
+use Psr\Http\Message\ServerRequestInterface;
+use League\OAuth2\Server\Exception\OAuthServerException;
+use League\OAuth2\Server\AuthorizationServer;
+
 class AuthorizationsController extends Controller
 {
     public function socialStore($type, SocialAuthorizationRequest $request)
@@ -61,6 +66,7 @@ class AuthorizationsController extends Controller
         return $this->respondWithToken($token)->setStatusCode(201);
     }
 
+    /*
     public function store(AuthorizationRequest $request)
     {
         $username = $request->username;
@@ -80,6 +86,17 @@ class AuthorizationsController extends Controller
 
         return $this->respondWithToken($token)->setStatusCode(201);
     }
+    */
+
+    //passport 认证
+    public function store(AuthorizationRequest $originRequest, AuthorizationServer $server, ServerRequestInterface $serverRequest)
+    {
+        try {
+            return $server->respondToAccessTokenRequest($serverRequest, new Psr7Response)->withStatus(201);
+        } catch(OAuthServerException $e) {
+            return $this->response->errorUnauthorized($e->getMessage());
+        }
+    }
 
     protected function respondWithToken($token)
     {
@@ -90,16 +107,31 @@ class AuthorizationsController extends Controller
         ]);
     }
 
-    public function update()
+//    public function update()
+//    {
+//        $token = \Auth::guard('api')->refresh();
+//        return $this->respondWithToken($token);
+//    }
+
+    public function update(AuthorizationServer $server, ServerRequestInterface $serverRequest)
     {
-        $token = \Auth::guard('api')->refresh();
-        return $this->respondWithToken($token);
+        try {
+            return $server->respondToAccessTokenRequest($serverRequest, new Psr7Response);
+        } catch(OAuthServerException $e) {
+            return $this->response->errorUnauthorized($e->getMessage());
+        }
     }
+
+
+//    public function destroy()
+//    {
+//        \Auth::guard('api')->logout();
+//        return $this->response->noContent();
+//    }
 
     public function destroy()
     {
-        \Auth::guard('api')->logout();
+        $this->user()->token()->revoke();
         return $this->response->noContent();
     }
-
 }
